a6aceffa55d11020bfa0fa047b1608c8a83a9a71
Sen, Puja <puja.sen@cgi.com>
2021-06-28 19:55:40 +0530

    Add project files.

---
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Models
+{
+    public class CourseForCreationDto : CourseForManipulationDto//, IValidatableObject
+    {
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //       yield return new ValidationResult("Description should be different from title",
+        //            new[] { "CourseForCreationDto" });
+        //    }
+        //}
+    }
+}
